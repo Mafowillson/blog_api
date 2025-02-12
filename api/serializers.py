c89af782f8d4de9f4c221473a11f0114bbd7b0b5
unknown <fouotsawilson@gmail.com>
2025-02-12 07:43:35 +0100

    firest_commit

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from blockapp.models import Post
+from rest_framework.serializers import ModelSerializer
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['uuid', 'title', 'content', 'created', 'updated']
+
